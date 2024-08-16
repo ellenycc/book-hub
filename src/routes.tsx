3a58f01bc6f8d4cb1eec7fb89dee383264a87590
Ellen CHan <ellenycc@gmail.com>
2024-08-16 15:52:16 +0100

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/Main";
 import BookDetailPage from "./pages/BookDetailPage";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "books/:id", element: <BookDetailPage /> },
