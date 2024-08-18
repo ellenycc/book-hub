be9bd7e502b74707ff1377e598ee81d5bc0aa209
Ellen CHan <ellenycc@gmail.com>
2024-08-18 07:39:06 +0100

    Fix sorting

--- src/services/books-url.ts
+++ src/services/books-url.ts
@@ -2,18 +2,14 @@ const API_KEY = "AIzaSyAje9Kn4hBLvO4yWkAX7BNGrHYpzB19jt0";
 const BASE_URL = "https://www.googleapis.com/books/v1/volumes";
 
 const getBooksUrl = (search: string, sortOrder?: string, id?: string) => {
-  const orderParam = sortOrder ? `&orderBy=${sortOrder}` : "";
-
   if (id) {
     return `${BASE_URL}/${id}?key=${API_KEY}`;
   }
 
-  let url = `${BASE_URL}?q=${search}`;
+  let url = `${BASE_URL}?q=${encodeURIComponent(search)}&key=${API_KEY}`;
 
-  if (search) {
-    url += `&key=${API_KEY}`;
-  } else if (sortOrder) {
-    url += `${orderParam}&key=${API_KEY}`;
+  if (sortOrder) {
+    url += `&orderBy=${sortOrder}`;
   }
 
   return url;
