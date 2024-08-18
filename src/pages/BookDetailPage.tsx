8470ece350d94c08e1f674c871807e15c902d4a2
Ellen CHan <ellenycc@gmail.com>
2024-08-18 15:06:19 +0100

    Adjust book detail page layout

--- src/pages/BookDetailPage.tsx
+++ src/pages/BookDetailPage.tsx
@@ -1,4 +1,11 @@
-import { Box, Heading, Spinner, Text, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Spinner,
+  Text,
+  Image,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useBook from "../hooks/useBook";
 import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
@@ -16,31 +23,35 @@ const BookDetailPage = () => {
   const description = book.volumeInfo.description;
 
   return (
-    <Box paddingX={5}>
-      <Heading>
-        {book.volumeInfo.title}: {book.volumeInfo.subtitle}
-      </Heading>
-      <Image
-        height="250px"
-        alignItems="center"
-        objectFit="contain"
-        marginTop={5}
-        src={
-          book.volumeInfo.imageLinks
-            ? book.volumeInfo.imageLinks.thumbnail
-            : noImage
-        }
-      />
-      <Text paddingTop={4} color="#718096"></Text>
-      <Text paddingTop={4} as="i">
-        By {book.volumeInfo.authors?.join(", ")}
-      </Text>
-
-      <Text
-        paddingTop={4}
-        dangerouslySetInnerHTML={{ __html: description }}
-      ></Text>
-    </Box>
+    <SimpleGrid columns={{ base: 1, md: 2 }} paddingX={5} spacing={2}>
+      <Box>
+        <Heading>
+          {book.volumeInfo.title}: {book.volumeInfo.subtitle}
+        </Heading>
+        <Text paddingTop={5} as="i">
+          By {book.volumeInfo.authors?.join(", ")}
+        </Text>
+        <Image
+          height="250px"
+          alignItems="center"
+          objectFit="contain"
+          borderRadius={4}
+          marginTop={5}
+          src={
+            book.volumeInfo.imageLinks
+              ? book.volumeInfo.imageLinks.thumbnail
+              : noImage
+          }
+        />
+      </Box>
+      <Box>
+        <Heading as="h2">Summary</Heading>
+        <Text
+          paddingTop={4}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      </Box>
+    </SimpleGrid>
   );
 };
 
