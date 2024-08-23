02840e6706a14b920ea5e9334c18d5530ab0ec6e
Ellen CHan <ellenycc@gmail.com>
2024-08-23 21:28:50 +0100

    Make navbar responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,37 @@
-import { Button, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  IconButton,
+  Image,
+} from "@chakra-ui/react";
 import logo from "../assets/book-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <HStack justifyContent="space-between" padding="20px" boxShadow="md">
+    <HStack
+      justifyContent="space-between"
+      padding={{ base: "10px", md: "20px" }}
+      boxShadow="md"
+      width="100%"
+    >
+      {/* Logo and Title */}
       <HStack spacing={4}>
         <Link to="/">
-          <Image src={logo} boxSize="50px" />
+          <Image src={logo} boxSize={{ base: "40px", md: "50px" }} />
         </Link>
-        <Heading fontSize="2xl">BookHub</Heading>
+        <Heading fontSize={{ base: "xl", md: "2xl" }}>BookHub</Heading>
       </HStack>
-      <HStack spacing={4}>
+
+      {/* Desktop Navigation */}
+      <HStack spacing={4} display={{ base: "none", md: "flex" }}>
         <Link to="/wishlist">
           <Button colorScheme="teal" variant="outline" size="md">
             Wish List
@@ -20,6 +39,46 @@ const NavBar = () => {
         </Link>
         <ColorModeSwitch />
       </HStack>
+
+      {/* Mobile Menu Button */}
+      <Box display={{ base: "block", md: "none" }}>
+        <IconButton
+          aria-label="Open Menu"
+          icon={<HamburgerIcon />}
+          variant="outline"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+      </Box>
+
+      {/* Mobile Navigation */}
+      {isMenuOpen && (
+        <Box
+          display={{ base: "flex", md: "none" }}
+          flexDirection="column"
+          position="absolute"
+          top="50px"
+          right="10px"
+          bg="white"
+          _dark={{ bg: "gray.800" }}
+          boxShadow="lg"
+          borderRadius="md"
+          zIndex={10}
+          p={4}
+        >
+          <Link to="/wishlist">
+            <Button
+              colorScheme="teal"
+              variant="outline"
+              width="100%"
+              marginBottom={2}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Wish List
+            </Button>
+          </Link>
+          <ColorModeSwitch />
+        </Box>
+      )}
     </HStack>
   );
 };
